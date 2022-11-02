c0b3f76fc0bdff63599f8a00495b539bfdd55ecc
Khizer Hussain <khizwaseem@gmail.com>
2022-11-02 23:24:01 +0500

    Configure react-app with redux for deployment to gh-pages : source code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions/actions";
 
 function App() {
+  const countState = useSelector((state) => state.numberChange);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="container">
+        <h1>Increment/Decrement Counter</h1>
+        <h4>made using react-redux</h4>
+
+        <div className='counting'>
+          <a className='minus' title='Decrement' onClick={() => dispatch(decNumber(1))}><span>-</span></a>
+          <input type="text" name="count" id="count" className='count_input' readOnly value={countState} />
+          <a className='plus' title='Increment' onClick={() => dispatch(incNumber(1))}><span>+</span></a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import numberChange from "./upDown";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    numberChange
+});
+
+export default rootReducer;
